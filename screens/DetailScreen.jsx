d9c97472a25c82766e07e4c943e8a4a251aeb41f
Alejandro Hernandez <pedrada914@gmail.com>
2025-08-08 18:36:45 -0600

    Adds dark mode support and caching
    Implements dark mode support using React Native's `useColorScheme` hook and custom themes.
    Adds caching mechanism for NASA APOD images using AsyncStorage to improve performance and reduce API calls.
    Improves the user interface with a new visual theme based on the system's color scheme.
    Adds Animated View for smoother transitions.

--- screens/DetailScreen.jsx
+++ screens/DetailScreen.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
+import { View, Text, Image, StyleSheet, ScrollView,useColorScheme } from "react-native";
+import { lightTheme, darkTheme } from "../theme/color";
 
 export default function DetailScreen({ route }) {
   const { item } = route.params;
+  const colorScheme = useColorScheme();
+  const theme = colorScheme === "dark" ? darkTheme : lightTheme;
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.title}>{item.title}</Text>
+    <ScrollView contentContainerStyle={[styles.container, { backgroundColor: theme.background }]}>
+      <Text style={[styles.title,{color:theme.text}]}>{item.title}</Text>
       <Image source={{ uri: item.hdurl || item.url }} style={styles.image} />
-      <Text style={styles.explanation}>{item.explanation}</Text>
+      <Text style={[styles.explanation,{color: theme.text}]}>{item.explanation}</Text>
     </ScrollView>
   );
 }
@@ -17,6 +20,7 @@ const styles = StyleSheet.create({
   container: {
     padding: 20,
     alignItems: "center",
+    height: "100%",
   },
   title: {
     fontSize: 22,
