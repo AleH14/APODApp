02377337d0f5c962ae926b04646319d01c22f620
Alejandro Hernandez <pedrada914@gmail.com>
2025-08-10 17:46:46 -0600

    Adds full screen image view with Wikipedia info
    Implements a new screen to display images in full screen.
    
    Fetches and displays related Wikipedia information for each image,
    enhancing user experience.
    
    Includes loading state and error handling for Wikipedia API calls.
    
    Adds SafeAreaProvider for consistent layout across devices.

--- App.js
+++ App.js
@@ -3,16 +3,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import DetailScreen from "./screens/DetailScreen";
+import FullScreenImage from "./screens/FullScreenImage";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <SafeAreaProvider>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Inicio" component={HomeScreen} />
         <Stack.Screen name="Detalles" component={DetailScreen} />
+        <Stack.Screen name="FullScreenImage" component={FullScreenImage} />
       </Stack.Navigator>
     </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
